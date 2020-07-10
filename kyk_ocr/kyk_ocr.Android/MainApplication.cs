b408940194abff376fc3650671aded439a800a5b
Steven Wilson <steven.wilson@accruent.com>
2020-07-10 15:59:59 +0100

    Started wiring in scanbot sdk for android

---
+++ kyk_ocr/kyk_ocr.Android/MainApplication.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Runtime;
+using Android.Util;
+using ScanbotSDK.Xamarin.Android;
+using ScanbotSDK.Xamarin.Forms;
+using System;
+using System.IO;
+using SBSDKConfiguration = ScanbotSDK.Xamarin.Forms.SBSDKConfiguration;
+
+namespace kyk_ocr.Droid
+{
+    public class MainApplication : Application
+    {
+        static string LOG_TAG = typeof(MainApplication).Name;
+        const string LICENSE_KEY = null;
+
+        public MainApplication(IntPtr javaReference, JniHandleOwnership transfer)
+               : base(javaReference, transfer) { }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            Log.Debug(LOG_TAG, "Initializing Scanbot SDK...");
+            var configuration = new SBSDKConfiguration
+            {
+                EnableLogging = true,
+                StorageBaseDirectory = GetDemoStorageBaseDirectory(),
+                DetectorType = DocumentDetectorType.MLBased
+            };
+            SBSDKInitializer.Initialize(this, LICENSE_KEY, configuration);
+        }
+
+        string GetDemoStorageBaseDirectory()
+        {
+            var directory = GetExternalFilesDir(null).AbsolutePath;
+            var externalPublicPath = Path.Combine(directory, "my-custom-storage");
+            Directory.CreateDirectory(externalPublicPath);
+            return externalPublicPath;
+        }
+    }
+}
